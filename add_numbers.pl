acb8977f988be189b9cc1bf076fee4d44bdc4f98
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-09-07 12:47:33 +0530

    added program for adding numbers

--- add_numbers.pl
+++ add_numbers.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/env swipl
 
+% function to add two numbers and store third variable.
 add(X, Y, SUM) :- SUM is X+Y.
-?- add(2, 3, RESULT), format('~10I', [RESULT]).
+
+?- add(1, 5, RESULT), format('~10I', [RESULT]).
 ?- nl.
 ?- halt(0).
